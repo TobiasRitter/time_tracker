e4e240c44205c6ab49d578c62f1e6b940ab3283b
Tobias Ritter <tobias.ritter@tum.de>
2023-02-28 23:46:33 +0100

    use google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const primaryColor = Color.fromARGB(255, 128, 128, 128);
 const textColor = Color.fromARGB(255, 96, 96, 96);
@@ -14,23 +15,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return MaterialApp(
       title: 'Time Tracker',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.black,
-        iconTheme: IconThemeData(color: textColor, size: 32.0),
-        textTheme: const TextTheme(
-            headlineLarge: TextStyle(
+        iconTheme: const IconThemeData(color: textColor, size: 32.0),
+        textTheme: GoogleFonts.oswaldTextTheme(textTheme).copyWith(
+          headlineLarge: GoogleFonts.oswald(
+            textStyle: const TextStyle(
               fontSize: 10000,
               color: Colors.white,
             ),
-            bodyLarge: const TextStyle(
+          ),
+          bodyLarge: GoogleFonts.oswald(
+            textStyle: const TextStyle(
               fontSize: 20,
               color: primaryColor,
             ),
-            bodyMedium: const TextStyle(
+          ),
+          bodyMedium: GoogleFonts.oswald(
+            textStyle: const TextStyle(
               color: textColor,
-            )),
+            ),
+          ),
+        ),
         colorScheme: ColorScheme.fromSwatch().copyWith(
           primary: primaryColor,
           secondary: secondaryColor,
@@ -199,14 +209,14 @@ class TimeRecord extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 4.0),
                   child: Text(
-                    "10:00 - 11:30",
+                    "1h 30min",
                     style: Theme.of(context).textTheme.bodyLarge,
                   ),
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 4.0),
                   child: Text(
-                    "01.01.2023",
+                    "10:00 - 11:30",
                     style: Theme.of(context).textTheme.bodyMedium,
                   ),
                 ),
