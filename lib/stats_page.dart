b6cb3ae40e6e6453a3e91592caff1f8dcfa7df5c
Tobias Ritter <tobias.ritter@tum.de>
2023-03-05 10:54:22 +0100

    use skeleton on stats page

--- lib/stats_page.dart
+++ lib/stats_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker/balance_entry.dart';
 import 'package:time_tracker/column_chart.dart';
-import 'package:time_tracker/hero_divider.dart';
 import 'package:time_tracker/nav_bar.dart';
+import 'package:time_tracker/skeleton.dart';
 
 class StatsPage extends StatelessWidget {
   final bool running;
@@ -20,6 +20,33 @@ class StatsPage extends StatelessWidget {
     Map<String, Duration> hourDifferences =
         workingHours.map((key, value) => MapEntry(key, value - goal));
 
+    var navBar = NavBar(
+      running: running,
+      left: () => Navigator.pop(context),
+      middle: () {},
+      iconLeft: Icons.arrow_back,
+      iconMiddle: const Icon(Icons.settings),
+      labelLeft: "Back",
+    );
+    var balances = Column(
+      children: const [
+        BalanceEntry(
+          title: "Week",
+          icon: Icons.calendar_today,
+          duration: Duration(hours: 1, minutes: 30),
+        ),
+        BalanceEntry(
+          title: "Month",
+          icon: Icons.calendar_month,
+          duration: Duration(hours: 2, minutes: 40),
+        ),
+        BalanceEntry(
+          title: "Total",
+          icon: Icons.watch_later_outlined,
+          duration: Duration(hours: 5, minutes: 10),
+        ),
+      ],
+    );
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -37,44 +64,14 @@ class StatsPage extends StatelessWidget {
         child: SafeArea(
           child: Center(
             child: Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: Column(children: [
-                Expanded(
-                  flex: 2,
-                  child: ColumnChart(hourDifferences: hourDifferences),
-                ),
-                const HeroDivider(),
-                Expanded(
-                  child: Column(
-                    children: const [
-                      BalanceEntry(
-                        title: "Week",
-                        icon: Icons.calendar_today,
-                        duration: Duration(hours: 1, minutes: 30),
-                      ),
-                      BalanceEntry(
-                        title: "Month",
-                        icon: Icons.calendar_month,
-                        duration: Duration(hours: 2, minutes: 40),
-                      ),
-                      BalanceEntry(
-                        title: "Total",
-                        icon: Icons.watch_later_outlined,
-                        duration: Duration(hours: 5, minutes: 10),
-                      ),
-                    ],
+                padding: const EdgeInsets.all(32.0),
+                child: Skeleton(
+                  upper: ColumnChart(
+                    hourDifferences: hourDifferences,
                   ),
-                ),
-                NavBar(
-                  running: running,
-                  left: () => Navigator.pop(context),
-                  middle: () {},
-                  iconLeft: Icons.arrow_back,
-                  iconMiddle: const Icon(Icons.settings),
-                  labelLeft: "Back",
-                ),
-              ]),
-            ),
+                  lower: balances,
+                  navBar: navBar,
+                )),
           ),
         ),
       ),
